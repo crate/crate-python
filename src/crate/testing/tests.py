23c8d4a415b14158f7567da5599ab19c99f77bf9
Sebastian Utz <su@rtme.net>
2013-09-03 17:11:36 +0200

    moved CrateLayer test to doctest file

--- src/crate/testing/tests.py
+++ src/crate/testing/tests.py
@@ -1,8 +1,6 @@
 import unittest
 import doctest
 import os
-import requests
-import random
 
 from .layer import CrateLayer
 
@@ -14,23 +12,16 @@ def crate_path(*parts):
     return docs_path('..', '..', 'parts', 'crate', *parts)
 
 
-def test_crate_layer():
-    """ Test the layer by starting it, issuing an request and stopping it.
+def setUp(test):
+    test.globs['crate_path'] = crate_path
 
-    >>> crate_layer =  CrateLayer('crate-test-'+str(random.randint(1, 10000)),
-    ...                          crate_home=crate_path(),
-    ...                          crate_exec=crate_path('bin', 'crate'),
-    ...                          port=19200,)
-    >>> crate_layer.start()
-    >>> res = requests.get('http://127.0.0.1:19200/_stats')
-    >>> res.status_code
-    200
-    >>> crate_layer.stop()
-
-    """
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(doctest.DocTestSuite())
-    return suite
 
+    s = doctest.DocFileSuite('layer.txt',
+                             setUp=setUp,
+                             optionflags=doctest.NORMALIZE_WHITESPACE |
+                             doctest.ELLIPSIS)
+    suite.addTest(s)
+    return suite

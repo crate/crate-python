9daf3b6fa2c97474f70d72987768f970c4e3a5c7
Mathias Fussenegger <f.mathias@zignar.net>
2013-10-01 14:56:15 +0200

    prepare release 0.0.6

--- src/crate/client/__init__.py
+++ src/crate/client/__init__.py
@@ -3,7 +3,7 @@ from .exceptions import Error
 
 # version string read from setup.py using a regex. Take care not to break the
 # regex!
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 apilevel = "2.0"
 threadsafety = 2

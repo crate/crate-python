0151ba997ec9f68332c150d09a2b969395dc77bf
Matthias Wahl <wahl@crate-technology.com>
2013-10-02 15:52:42 +0200

    update version

--- src/crate/client/__init__.py
+++ src/crate/client/__init__.py
@@ -3,7 +3,7 @@ from .exceptions import Error
 
 # version string read from setup.py using a regex. Take care not to break the
 # regex!
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 apilevel = "2.0"
 threadsafety = 2

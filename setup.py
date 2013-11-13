2846eb445bb3a7634980e3eb6403079fcf5453c4
Matthias Wahl <wahl@crate-technology.com>
2013-11-13 09:20:18 +0100

    fixup: version pinnging

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ import re
 
 
 requirements = [
-    'requests==2.0.1',
-    'PrettyTable==0.7.2',
-    'appdirs==1.2.0',
+    'requests>=2.0.1,<=3.0',
+    'PrettyTable>=0.7,<=0.8',
+    'appdirs>=1.2,<=2.0',
 ]
 
 if (2, 6) == sys.version_info[:2]:
-    requirements.append('argparse==1.2.1')
+    requirements.append('argparse>=1.2.1')
 
 
 def read(path):
@@ -58,10 +58,10 @@ setup(
     },
     extras_require=dict(
         test=['lovely.testlayers',
-              'mock==1.0.1',
+              'mock>=1.0.1',
               'zope.testing',
-              'zc.customdoctests==1.0.1'],
-        sqlalchemy=['sqlalchemy==0.8.2']
+              'zc.customdoctests>=1.0.1'],
+        sqlalchemy=['sqlalchemy>=0.8.2,<=0.9']
     ),
     install_requires=requirements,
     package_data={'': ['*.txt']},

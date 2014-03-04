2cea1841820f954da29c353ff86d096db097ce7e
Matthias Wahl <matthias@crate.io>
2014-03-04 12:17:52 +0100

    made bundled zipfile executable and build bundle in virtualenv

--- devtools/bundle.sh
+++ devtools/bundle.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+BUNDLE_FILE='crash.zip.py'
+VENV_DIR='venv'
+
+if [[ -e ${BUNDLE_FILE} ]]; then
+    rm ${BUNDLE_FILE}
+fi
 
 if [[ -d './dependencies' ]]; then
     rm -rf ./dependencies/*
@@ -9,8 +15,15 @@ if [[ -d './eggs' ]]; then
     rm -rf ./eggs
 fi
 
-python bootstrap.py
-bin/buildout -N
+if [[ -z `which virtualenv` ]]; then
+    echo "virtualenv not found. please install virtualenv"
+    exit 1
+fi
+virtualenv --no-site-packages --clear ${VENV_DIR}
+PYTHON_EXECUTABLE=./${VENV_DIR}/bin/python
+./${VENV_DIR}/bin/easy_install readline
+${PYTHON_EXECUTABLE} bootstrap.py
+${PYTHON_EXECUTABLE} bin/buildout -N
 
 if [[ ! -d './dependencies' ]]; then
     mkdir "dependencies"
@@ -29,4 +42,8 @@ echo "from crate.client.crash import main; main()" > dependencies/__main__.py
 cd dependencies
 zip -r crate.zip ./*
 cd ../
-mv dependencies/crate.zip crash.zip.py
+mv dependencies/crate.zip ${BUNDLE_FILE}
+
+echo "#!/usr/bin/env python" | cat - ${BUNDLE_FILE} > tmpfile
+mv tmpfile ${BUNDLE_FILE}
+chmod u+x ${BUNDLE_FILE}

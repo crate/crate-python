0f7fdfbceb318d85d2fff33e91ee9c873428a619
Christian Haudum <christian.haudum@gmail.com>
2015-08-12 15:28:46 +0200

    prepare release 0.13.4

--- src/crate/client/__init__.py
+++ src/crate/client/__init__.py
@@ -24,7 +24,7 @@ from .exceptions import Error
 
 # version string read from setup.py using a regex. Take care not to break the
 # regex!
-__version__ = "0.13.3"
+__version__ = "0.13.4"
 
 apilevel = "2.0"
 threadsafety = 2

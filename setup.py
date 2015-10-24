2c02227d087a0237f3276d75da0bd530b9886343
Mathias Fussenegger <f.mathias@zignar.net>
2015-10-24 19:10:53 +0200

    remove lovely.testlayers dependency
    The functionality that was used of the ServerLayer is
    basically just subprocess Popen and kill.
    
    Everything else was either un-used (e.g. stdout/stderr
    handling) or unneeded (waiting for the process to become
    responsive on its port - the crate layer has that
    functionality already with the addition of waiting for HTTP
    200 status codes)

--- setup.py
+++ setup.py
@@ -71,8 +71,7 @@ setup(
         ]
     },
     extras_require=dict(
-        test=['lovely.testlayers',
-              'mock>=1.0.1',
+        test=['mock>=1.0.1',
               'zope.testing',
               'zc.customdoctests>=1.0.1'],
         sqlalchemy=['sqlalchemy>=0.8.2']

f2a7979640a5d3e5be61cce1c3802d5cecf1bf10
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-04-29 17:28:24 +0200

    Config update for crate-docs-theme 0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,5 @@
-from crate.theme.rtd.conf.clients_python import *
+from crate.theme.rtd.conf.python import *
 html_favicon = None
+
+site_url = 'https://crate.io/docs/python/en/latest/'
+extensions = ['sphinx_sitemap']

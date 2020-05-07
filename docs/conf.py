8b052a1b00074300a3d3cc22c3d4e2b7a5005fde
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-05-07 15:50:34 +0200

    Prune config (no longer needed)

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,4 @@
 from crate.theme.rtd.conf.python import *
-html_favicon = None
-
-site_url = 'https://crate.io/docs/python/en/latest/'
-extensions = ['sphinx_sitemap']
 
 rst_prolog = """
 .. |nbsp| unicode:: 0xA0

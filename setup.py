1f3189e227b013ae92f6380fbed1da517a293db8
Mika Naylor <mail@autophagy.io>
2020-07-10 10:57:59 +0200

    Added support for geo_point and geo_shape to the SQLAlchemy dialect
    This should address #273

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     extras_require=dict(
         test=['zope.testing',
               'zc.customdoctests>=1.0.1'],
-        sqlalchemy=['sqlalchemy>=1.0,<1.4']
+        sqlalchemy=['sqlalchemy>=1.0,<1.4', 'geojson>=2.5.0']
     ),
     python_requires='>=3.4',
     install_requires=requirements,
--- src/crate/client/sqlalchemy/types.py
+++ src/crate/client/sqlalchemy/types.py
@@ -24,6 +24,8 @@ from sqlalchemy.sql import operators, expression
 from sqlalchemy.sql import default_comparator
 from sqlalchemy.ext.mutable import Mutable
 
+import geojson
+
 
 class MutableList(Mutable, list):
 
@@ -217,3 +219,46 @@ class _ObjectArray(sqltypes.UserDefinedType):
 
 
 ObjectArray = MutableList.as_mutable(_ObjectArray)
+
+
+class Geopoint(sqltypes.UserDefinedType):
+
+    class Comparator(sqltypes.TypeEngine.Comparator):
+
+        def __getitem__(self, key):
+            return default_comparator._binary_operate(self.expr,
+                                                      operators.getitem,
+                                                      key)
+
+    def get_col_spec(self):
+        return 'GEO_POINT'
+
+    def bind_processor(self, dialect):
+        def process(value):
+            if isinstance(value, geojson.Point):
+                return value.coordinates
+            return value
+        return process
+
+    def result_processor(self, dialect, coltype):
+        return tuple
+
+    comparator_factory = Comparator
+
+
+class Geoshape(sqltypes.UserDefinedType):
+
+    class Comparator(sqltypes.TypeEngine.Comparator):
+
+        def __getitem__(self, key):
+            return default_comparator._binary_operate(self.expr,
+                                                      operators.getitem,
+                                                      key)
+
+    def get_col_spec(self):
+        return 'GEO_SHAPE'
+
+    def result_processor(self, dialect, coltype):
+        return geojson.GeoJSON.to_instance
+
+    comparator_factory = Comparator

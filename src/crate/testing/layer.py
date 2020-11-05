c87dc71715bb5451c15f9d00d3f85cd1c4e1d968
Andreas Motl <andreas.motl@crate.io>
2020-11-05 15:06:12 +0100

    CI: Improve logging to find out about any errors when starting CrateDB

--- src/crate/testing/layer.py
+++ src/crate/testing/layer.py
@@ -297,6 +297,7 @@ class CrateLayer(object):
 
     def start(self):
         self._clean()
+        log.info("Starting process '{}'".format(self.start_cmd))
         self.process = subprocess.Popen(self.start_cmd,
                                         env=self.env,
                                         stdout=subprocess.PIPE)

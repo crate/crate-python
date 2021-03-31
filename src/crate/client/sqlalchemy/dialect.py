1c460792ae40ca04e2954811b7354d8a56fd416e
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-03-31 14:08:00 -0600

    Validate date type in processors

--- src/crate/client/sqlalchemy/dialect.py
+++ src/crate/client/sqlalchemy/dialect.py
@@ -89,6 +89,8 @@ class Date(sqltypes.Date):
         def process(value):
             if not value:
                 return
+            if isinstance(value, datetime):
+                return value.date()
             try:
                 return datetime.utcfromtimestamp(value / 1e3).date()
             except TypeError:
@@ -128,6 +130,8 @@ class DateTime(sqltypes.DateTime):
         def process(value):
             if not value:
                 return
+            if isinstance(value, datetime):
+                return value
             try:
                 return datetime.utcfromtimestamp(value / 1e3)
             except TypeError:

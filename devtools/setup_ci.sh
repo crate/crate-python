6150a63ceeb4b8f8e9cf925a5c50cd3eb94ae5f8
Andreas Motl <andreas.motl@crate.io>
2022-06-17 22:00:35 +0200

    CI: Increase verbosity level for `buildout`

--- devtools/setup_ci.sh
+++ devtools/setup_ci.sh
@@ -62,8 +62,10 @@ function main() {
     sed -ir "s/crate_server.*/crate_server = ${cratedb_version}/g" versions.cfg
   fi
 
+  # Switch to enable compatibility with older versions of macOS.
   export SYSTEM_VERSION_COMPAT=1
-  buildout -n -c base.cfg
+
+  buildout -vvv -n -c base.cfg
 
 }
 

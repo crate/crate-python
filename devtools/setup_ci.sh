fc38073d48f36026c0c283a4fcec224770ac7531
Andreas Motl <andreas.motl@crate.io>
2022-06-19 02:17:41 +0200

    CI: Use environment variables `CRATEDB_VERSION` and `SQLALCHEMY_VERSION`
    Instead of using GNU getopt in the `setup_ci.sh` program, the needed
    information is propagated using environment variables.

--- devtools/setup_ci.sh
+++ devtools/setup_ci.sh
@@ -2,59 +2,31 @@
 
 set -e
 
-function args() {
-  options=$(getopt --long cratedb-version: --long sqlalchemy-version: -- "$@")
-  [ $? -eq 0 ] || {
-    echo "Incorrect options provided"
-    exit 1
-  }
-  eval set -- "$options"
-  while true; do
-      case "$1" in
-      --cratedb-version)
-          shift;
-          cratedb_version=$1
-          ;;
-      --sqlalchemy-version)
-          shift;
-          sqlalchemy_version=$1
-          ;;
-      --)
-          shift
-          break
-          ;;
-      esac
-      shift
-  done
-}
-
 function main() {
 
-  # Read command line arguments.
-  args $0 "$@"
-
   # Sanity checks.
-  [ -z ${cratedb_version} ] && {
+  [ -z ${CRATEDB_VERSION} ] && {
     echo "--cratedb-version must be given"
+    echo "Environment variable 'CRATEDB_VERSION' needed"
     exit 1
   }
-  [ -z ${sqlalchemy_version} ] && {
-    echo "--sqlalchemy-version must be given"
+  [ -z ${SQLALCHEMY_VERSION} ] && {
+    echo "Environment variable 'SQLALCHEMY_VERSION' needed"
     exit 1
   }
 
   # Let's go.
-  echo "Invoking tests with CrateDB ${cratedb_version} and SQLAlchemy ${sqlalchemy_version}"
+  echo "Invoking tests with CrateDB ${CRATEDB_VERSION} and SQLAlchemy ${SQLALCHEMY_VERSION}"
 
   # Install designated SQLAlchemy version.
-  pip install "sqlalchemy==${sqlalchemy_version}"
+  pip install "sqlalchemy==${SQLALCHEMY_VERSION}"
 
   # Replace CrateDB version.
-  if [ ${cratedb_version} = "nightly" ]; then
+  if [ ${CRATEDB_VERSION} = "nightly" ]; then
     sed -ir "s/releases/releases\/nightly/g" buildout.cfg
     sed -ir "s/crate_server.*/crate_server = latest/g" versions.cfg
   else
-    sed -ir "s/crate_server.*/crate_server = ${cratedb_version}/g" versions.cfg
+    sed -ir "s/crate_server.*/crate_server = ${CRATEDB_VERSION}/g" versions.cfg
   fi
 
 }

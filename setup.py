5274c52fa6fba2e3442a470ca3e59f2225e4b4b1
Andreas Motl <andreas.motl@crate.io>
2024-01-18 19:03:09 +0100

    Tests: Update to pandas 2.1

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
               'dask',
               'stopit>=1.1.2,<2',
               'flake8>=4,<8',
-              'pandas<2.1',
+              'pandas<2.2',
               'pytz',
               ],
         doc=['sphinx>=3.5,<8',

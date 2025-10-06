371e12576f508d1250b267690237e07779c3c935
Ivan <surister98@gmail.com>
2025-10-06 11:04:22 +0200

    Increase wait time threshold for Crate instance startup
    Increased the wait time threshold from 30 to 60 seconds before raising an error.

--- src/crate/testing/layer.py
+++ src/crate/testing/layer.py
@@ -387,7 +387,7 @@ class CrateLayer:
                 self.stop()
                 raise e
 
-            if wait_time > 30:
+            if wait_time > 60:
                 for line in line_buf.lines:
                     log.error(line)
                 self.stop()

bf2903a4c40adbb4728801787efed99ee39e4a6c
surister <surister98@gmail.com>
2025-10-23 18:02:46 +0200

    Migrate test_connection

--- tests/client/test_connection.py
+++ tests/client/test_connection.py
@@ -52,73 +52,72 @@ def test_context_manager():
         close_func.assert_called_once()
 
 
-class ConnectionTest(TestCase):
-    def test_connection_mock(self):
-        """
-        For testing purposes it is often useful to replace the client used for
-        communication with the CrateDB server with a stub or mock.
+def test_connection_mock():
+    """
+    Verify that a custom client can be passed.
 
-        This can be done by passing an object of the Client class when calling
-        the `connect` method.
-        """
 
-        class MyConnectionClient:
-            active_servers = ["localhost:4200"]
+    For testing purposes, it is often useful to replace the client used for
+    communication with the CrateDB server with a stub or mock.
 
-            def __init__(self):
-                pass
+    This can be done by passing an object of the Client class when calling
+    the `connect` method.
+    """
 
-            def server_infos(self, server):
-                return ("localhost:4200", "my server", "0.42.0")
+    mock = MagicMock(spec=Client)
+    mock.server_infos.return_value = "localhost:4200", "my server", "0.42.0"
+    connection = connect(crate_host, client=mock)
 
-        connection = connect([crate_host], client=MyConnectionClient())
-        self.assertIsInstance(connection, Connection)
-        self.assertEqual(
-            connection.client.server_infos("foo"),
-            ("localhost:4200", "my server", "0.42.0"),
-        )
+    assert isinstance(connection, Connection)
+    assert connection.client.server_infos("foo") == ("localhost:4200", "my server", "0.42.0")
 
-    def test_with_timezone(self):
-        """
-        The cursor can return timezone-aware `datetime` objects when requested.
 
-        When switching the time zone at runtime on the connection object, only
-        new cursor objects will inherit the new time zone.
-        """
+def test_with_timezone():
+    """
+    Verify the logic of passing timezone objects to the client.
 
-        tz_mst = datetime.timezone(datetime.timedelta(hours=7), name="MST")
-        connection = connect("localhost:4200", time_zone=tz_mst)
-        cursor = connection.cursor()
-        self.assertEqual(cursor.time_zone.tzname(None), "MST")
-        self.assertEqual(
-            cursor.time_zone.utcoffset(None), datetime.timedelta(seconds=25200)
-        )
+    The cursor can return timezone-aware `datetime` objects when requested.
 
-        connection.time_zone = datetime.timezone.utc
-        cursor = connection.cursor()
-        self.assertEqual(cursor.time_zone.tzname(None), "UTC")
-        self.assertEqual(
-            cursor.time_zone.utcoffset(None), datetime.timedelta(0)
-        )
+    When switching the time zone at runtime on the connection object, only
+    new cursor objects will inherit the new time zone.
 
-    def test_timeout_float(self):
-        """
-        Verify setting the timeout value as a scalar (float) works.
-        """
-        with connect("localhost:4200", timeout=2.42) as conn:
-            self.assertEqual(conn.client._pool_kw["timeout"], 2.42)
+    These tests are complementary to timezone `test_cursor`
+    """
 
-    def test_timeout_string(self):
-        """
-        Verify setting the timeout value as a scalar (string) works.
-        """
-        with connect("localhost:4200", timeout="2.42") as conn:
-            self.assertEqual(conn.client._pool_kw["timeout"], 2.42)
+    tz_mst = datetime.timezone(datetime.timedelta(hours=7), name="MST")
+    connection = connect("localhost:4200", time_zone=tz_mst)
+    cursor = connection.cursor()
 
-    def test_timeout_object(self):
-        """
-        Verify setting the timeout value as a Timeout object works.
-        """
-        timeout = Timeout(connect=2.42, read=0.01)
-        with connect("localhost:4200", timeout=timeout) as conn:
-            self.assertEqual(conn.client._pool_kw["timeout"], timeout)
+    assert cursor.time_zone.tzname(None) == "MST"
+    assert cursor.time_zone.utcoffset(None) == datetime.timedelta(seconds=25200)
+
+    connection.time_zone = datetime.timezone.utc
+    cursor = connection.cursor()
+    assert cursor.time_zone.tzname(None) == "UTC"
+    assert cursor.time_zone.utcoffset(None) == datetime.timedelta(0)
+
+
+
+def test_timeout_float():
+    """
+    Verify setting the timeout value as a scalar (float) works.
+    """
+    with connect("localhost:4200", timeout=2.42) as conn:
+        assert conn.client._pool_kw["timeout"] == 2.42
+
+
+def test_timeout_string():
+    """
+    Verify setting the timeout value as a scalar (string) works.
+    """
+    with connect("localhost:4200", timeout="2.42") as conn:
+        assert conn.client._pool_kw["timeout"] == 2.42
+
+
+def test_timeout_object():
+    """
+    Verify setting the timeout value as a Timeout object works.
+    """
+    timeout = Timeout(connect=2.42, read=0.01)
+    with connect("localhost:4200", timeout=timeout) as conn:
+        assert conn.client._pool_kw["timeout"] == timeout

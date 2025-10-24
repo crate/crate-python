08e9401783723b90072182beda24109de3c9270c
surister <surister98@gmail.com>
2025-10-24 13:32:39 +0200

    Migrate test_http and fix some test_cursor

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
+import multiprocessing
+import socket
+import threading
+from contextlib import contextmanager
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
 import urllib3
 from unittest.mock import MagicMock
 
@@ -36,3 +42,57 @@ def mocked_connection():
             mocked_connection.client.sql.assert_called_once_with(statement, 1, None)
     """
     yield crate.client.connect(client=MagicMock(spec=crate.client.http.Client))
+
+
+
+
+@pytest.fixture
+def serve_http():
+    """
+    Returns a context manager that start an http server running in another thread that returns
+    CrateDB successful responses.
+
+    It accepts an optional parameter, the handler class, it has to be an instance of `BaseHTTPRequestHandler`
+    The port will be an unused random port.
+
+    Example:
+        def test_http(serve_http):
+            with serve_http() as url:
+                urllib3.urlopen(url)
+
+    See `test_http.test_keep_alive` for more advance example.
+    """
+
+    @contextmanager
+    def _serve(handler_cls=BaseHTTPRequestHandler):
+        assert issubclass(handler_cls, BaseHTTPRequestHandler)
+        sock = socket.socket()
+        sock.bind(("127.0.0.1", 0))
+        host, port = sock.getsockname()
+        sock.close()
+
+        manager = multiprocessing.Manager()
+        SHARED = manager.dict()
+        SHARED["count"] = 0
+        SHARED["usernameFromXUser"] = None
+        SHARED["username"] = None
+        SHARED["password"] = None
+        SHARED["schema"] = None
+
+        server = HTTPServer((host, port), handler_cls)
+
+        server.SHARED = SHARED
+
+        # We could use a process, but starting a thread is faster.
+        # process = ForkProcess(target=server.serve_forever)
+        # process.start()
+
+        thread = threading.Thread(target=server.serve_forever, daemon=False)
+        thread.start()
+        try:
+            yield server, f"http://{host}:{port}"
+
+        finally:
+            server.shutdown()
+            thread.join()
+    return _serve
